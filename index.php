f6ae28e06732c15d4ae53bdd33b28e029620fb71
Andrea Perna <xnarctic@gmail.com>
2024-02-14 20:47:35 +0100

    Unit10 Complete

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-try{
-$dbh = new PDO("mysql:host=mariadb;dbname=ShoppingCart", "root", "root");
-$sth = $dbh->query("SELECT * From product");
-var_dump($sth->fetchAll())
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
